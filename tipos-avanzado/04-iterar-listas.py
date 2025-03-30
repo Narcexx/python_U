f1b62245f363a52323173abad9fa2b778f6c1113
Marce <manu.rubilar19@gmail.com>
2025-03-30 00:28:51 -0300

    first commit

--- tipos-avanzado/04-iterar-listas.py
+++ tipos-avanzado/04-iterar-listas.py
@@ -3,5 +3,5 @@ mascotas = ["Lucy", "Nana", "Garu"]
 # for mascota in mascotas:
 #     print(mascota)
 
-for indice, mascota in enumerate(mascotas):
+for indice, mascota in enumerate(mascotas): # Sirve para enumerar la lista puede poner , start=1 para que empiece desde el 1
     print(indice, mascota) 
